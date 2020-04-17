499d1d414e53ccc8072b7f730083f110dc8e9700
Michael Bidnyk <mbidnyk1@gmail.com>
2020-04-16 22:24:31 -0500

    Fetching data from api and displaying questions and shuffled answers

--- react-redux-app/src/store/reducers/triviaReducer.js
+++ react-redux-app/src/store/reducers/triviaReducer.js
@@ -1,30 +1,39 @@
+import  {FETCH_TRIVIA_START,FETCH_TRIVIA_SUCCESS, FETCH_TRIVIA_FAILURE, SET_TRIVIA_ANSWERS} from '../actions/actionTypes'
+
+
 const initialState = {
     triviaData: {},
+    triviaAnswers: [],
     isFetching: false,
         error: ''
 }
 
 export const triviaReducer = (state = initialState, action) => {
-    console.log('question',state.triviaData)
+    console.log('triviaData Question',state.triviaData)
     console.log('payload',action.payload)
     switch (action.type) {
-        case 'FETCH_TRIVIA_START':
+        case FETCH_TRIVIA_START:
             return {
                 ...state,
                 isFetching: true
             };
-        case 'FETCH_TRIVIA_SUCCESS':
+        case FETCH_TRIVIA_SUCCESS:
             return {
                 ...state,
-                 triviaData: [...state.triviaData, action.payload],
+                 triviaData: action.payload,
                 isFetching: false,
                 error:''
             };
-        case 'FETCH_QUOTE_FAILURE':
+        case FETCH_TRIVIA_FAILURE:
             return {
                 ...state,
                 isFetching: false,
                 error: action.payload
+            };
+        case SET_TRIVIA_ANSWERS:
+            return {
+            ...state,
+            triviaAnswers: [...state.triviaData.incorrect_answers, state.triviaData.correct_answer]
             };
         default:
             return state;
